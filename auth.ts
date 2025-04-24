72203256653d3a5dde3b7cf3e005e47363102d0e
liyxng04@gmail.com <jli26@bu.edu>
2025-04-24 15:15:49 -0400

    commited the avatar

--- auth.ts
+++ auth.ts
@@ -2,11 +2,28 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, auth } = NextAuth({
-    providers: [
-      GitHub({
-        clientId: process.env.GITHUB_CLIENT_ID!,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      }),
-    ],
-    secret: process.env.AUTH_SECRET,
-  });
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    async jwt({ token, profile }) {
+      if (profile) {
+        token.username = profile.login;
+        token.avatar = profile.avatar_url; // Save the avatar URL
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.username = token.username as string;
+      session.avatar = token.avatar as string; // Save the avatar URL to the session
+      return session;
+    },
+  },
+});
---
+++ types/next-auth.d.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+    interface Session {
+      username: string;
+      avatar: string;  
+    }
+  
+    interface JWT {
+      username: string;
+      avatar: string;  
+    }
+  }
+  
